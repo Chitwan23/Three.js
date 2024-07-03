fbf916167d7351ccd4411030477887cd293e49b7
Chitwan23 <singhchitwan08@gmail.com>
2024-07-04 00:41:55 +0530

    changes added

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   assetsInclude: ['**/*.glb'],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,4 +13,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
+  base: '/Three.js/', // Add this line
 })
+
