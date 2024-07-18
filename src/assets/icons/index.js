095e267b7f5cc1ece207e8d27d4bd644dfb0a89e
Chitwan23 <singhchitwan08@gmail.com>
2024-07-18 23:41:13 +0530

    ..

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -6,7 +6,6 @@ import html from './html.svg'
 import javascript from './javascript.svg'
 import mongodb from './mongodb.svg'
 import motion from './motion.svg'
-// import mui from './mui.svg'
 import nextjs from './nextjs.svg'
 import nodejs from './nodejs.svg'
 import react from './react.svg'
@@ -16,8 +15,6 @@ import tailwindcss from './tailwindcss.svg'
 import typescript from './typescript.svg'
 import linkedin from './linkedin.svg'
 import twitter from './twitter.svg'
-import car from './car.svg'
-import estate from './estate.svg'
 import pricewise from './pricewise.svg'
 import snapgram from './snapgram.svg'
 import summiz from './summiz.svg'
@@ -43,7 +40,6 @@ export {
     javascript,
     mongodb,
     motion,
-    // mui,
     nextjs,
     nodejs,
     react,
@@ -53,8 +49,6 @@ export {
     typescript,
     linkedin,
     twitter,
-    car,
-    estate,
     pricewise,
     snapgram,
     summiz,
